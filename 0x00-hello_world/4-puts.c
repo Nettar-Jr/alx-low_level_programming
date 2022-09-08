aa73328c864659a72561742f5067825c4019d591
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-08 14:23:35 -0700

    a C program that prints specific text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
-
-
-
-/**
- ** Main - Entry point
- **
- ** Return: Always 0 (Success)
- *
-*/
-int main(void)	
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

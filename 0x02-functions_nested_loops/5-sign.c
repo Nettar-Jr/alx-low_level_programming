29d28a50402161fea302dddf4c1c936b167a6943
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-14 07:18:12 -0700

    A unction that prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if(n > 0)
 	{
-		_putchar("+, %u", n);
+		printf("+, %u", n);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar("0, %u", n);
+		printf("0, %u", n);
 		retunr (0);
 	}
 	else
 	{
-		_putchar("-, %u", n);
+		printf("-, %u", n);
 		retunr (-1);
 	}
 }

0794825348f6e9f9eb5ac0bf2e2ad1084c21a5e1
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-16 18:16:57 -0700

    A functiont that finds the prime numbers of a number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ ** main - Entry point
+ *
+ ** Return: Always 0
+ **/
+
+int main(void)
+{
+	unsigned long int i, num = 612852475143;
+
+	for (i = 0; i < 12057; i += 2)
+
+	{
+		while (num % i == 0 && num != i)
+			num /= i;
+	}
+	printf("%lu\n", num);
+
+	return (0);
+}

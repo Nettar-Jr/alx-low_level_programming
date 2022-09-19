b4f5f8275ee47e297abb0ff99f569c49c51f0a2c
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-19 16:34:41 -0700

    A function that prints the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s : string to be counted
+ *
+ * Retrun: 0
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	
+	len = strlen(s);
+	return (len);
+}	

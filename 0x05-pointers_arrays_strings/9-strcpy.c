bdc1b8609d25b4284a1d5a6c92aca245f1633f90
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-19 19:57:25 -0700

    A function that copies one array into another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copy element from one array to another
+ * @src : array source
+ * @dest : array destination
+ *
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}

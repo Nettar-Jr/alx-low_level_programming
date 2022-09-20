6d5728af007131a8fda3888f9be8f401830ab7e6
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-19 20:29:18 -0700

    A function that converts strings to integers uisng atoi from stdlib.h

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi : converts strings to integers
+ * @s string to be converted
+ *
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int x = atoi(s);
+
+	return (x);
+}

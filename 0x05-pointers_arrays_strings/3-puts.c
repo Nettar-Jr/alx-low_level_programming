8b71303f8f59900f0b2f14e3184611aa434bf132
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-19 17:02:59 -0700

    A program that prints a string using _putchar

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/*
+ * _puts - prints out a string and \n
+ * @str : string to be printed
+ *
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	int len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}

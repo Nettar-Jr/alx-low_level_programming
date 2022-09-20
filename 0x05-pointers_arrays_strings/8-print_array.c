fc444c50cf0bf8fe3922baad602c0d46b5feb6e1
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-19 19:44:50 -0700

    A function that prints the element of an array with space and comma separation

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,18 +5,18 @@
  * print_array - print the n element in an arrray
  * @a : array to be printed
  * @n : number if elements to be printed
- * 
+ *
  * Return: 0
  */
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= (n-1); i++)
+	for (i = 0; i <= (n - 1); i++)
 	{
 		printf("%d", a[i]);
 
-		if (i != (n-1))
+		if (i != (n - 1))
 			printf(", ");
 	}
 	printf("\n");

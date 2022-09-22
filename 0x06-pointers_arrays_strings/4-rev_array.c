9f83e07d49071b95a306877575fac0b45e0d8767
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-22 04:53:54 -0700

    A function that reverse the int in an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,11 +2,11 @@
 #include <string.h>
 
 /**
-* _reverse_array - reverse the ints in an array
-* 
+* reverse_array - reverse the ints in an array
+*
 * @a: array to be reversed
 * @n: numbers in array
-* 
+*
 * Return: 0
 */
 
@@ -15,10 +15,10 @@ void reverse_array(int *a, int n)
 	int temp;
 	int i;
 
-	for (i = 0; i<n/2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		temp = a[i];
-		a[i] = a[n-i-1];
-		a[n-i-1] = temp;
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
 	}
 }

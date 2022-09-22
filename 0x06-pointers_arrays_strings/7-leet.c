47fdf6e4d503b5637be220e40bb270564dcd6c59
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-22 13:40:29 -0700

    A function that encode a set of string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - encodes strings to numbers
+ *
+ * @str: string to be encoded
+ *
+ * Return: 0;
+ */
+
+char *leet(char *str)
+{
+	int i = 0, j;
+	char m[] = "aAeEoOtTlL";
+	char n[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (; str[i] != '\0'; i++)
+		{
+			for (j = 0; j <= 9; j++)
+			{
+				if (m[j] == str[i])
+				{
+					str[i] = n[j];
+				}
+			}
+		}
+	}
+	return (str);
+}

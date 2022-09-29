c90c15fd11de6e1d9c20f6c5f0d85a727f728347
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-28 21:21:22 -0700

    A function that multiple x int y number of times

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - x raised to the power of y
+ *
+ * @x: the nunber
+ * @y: the power
+ *
+ * Return: x
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}

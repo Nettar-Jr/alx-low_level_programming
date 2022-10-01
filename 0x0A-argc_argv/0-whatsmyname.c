ca508b1228847c8ee982e756722e171e7136eede
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-30 18:00:13 -0700

    A function that prints the first argument in a commandline argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,9 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc  __attribute__((unused)), char *argv[])
 {
 	printf("This program was called with %s\n", argv[0]);
+
 	return (0);
 }

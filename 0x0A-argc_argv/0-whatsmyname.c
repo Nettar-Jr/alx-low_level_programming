61e62f665287585e5a0a3706120f014f88a0720c
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-30 18:28:21 -0700

    A function that prints the first argument in a commandline argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-#include "main.h"
-
-void print_char(char *s);
 
 /**
  * main - print firts argument
@@ -14,27 +11,8 @@ void print_char(char *s);
 
 int main(int argc  __attribute__((unused)), char *argv[])
 {
-	print_char(argv[0]);
+	printf("%s", argv[0]);
 	putchar('\n');
 
 	return (0);
-}
-
-/**
- * print_char -  print chars of string
- *
- * @s: string to be printed
- *
- * Return: 0
- */
-
-void print_char(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		putchar(s[i]);
-		i++;
-	}
 }

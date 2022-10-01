0cb27132cd52fe5f84a016233bddb3d3f2229eb4
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-09-30 19:15:15 -0700

    A function that prints all arguments of the commandline

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all argv arguments
+ *
+ * @argc: number of argument
+ * @argv: array pointer to all argument
+ *
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}

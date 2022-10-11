b4cd01252a325edec88fc0388cd541ed2cd991c0
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-10-11 16:11:44 -0700

    A header file with a type struct dog and elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+/**
+ * struct dog - a dog structure
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*STRUCT_DOG*/

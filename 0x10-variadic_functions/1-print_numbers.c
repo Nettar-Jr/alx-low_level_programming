39cc16f4da53c08e12c63515672ed6fce024e489
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-10-14 13:12:11 -0700

    A function that prints out int passed as arguments

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - print out numbers with character inbetween
+ *
+ * @separator: number separator (comma)
+ * @n: number of arg to be printed
+ *
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	int num;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			num = va_arg(args, int);
+			printf("%d", num);
+		}
+		else
+		{
+			num =  va_arg(args, int);
+			printf("%d", num);
+			printf("%s", separator);
+		}
+	}
+
+	printf("\n");
+}

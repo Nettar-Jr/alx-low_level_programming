032292a43f42d35073e2f730eea2bd7c5b03c9f9
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-10-14 13:43:19 -0700

    A function that prints out int passed as arguments

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,10 +20,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n && n != 0; i++)
 	{
-			printf("%d", va_arg(args, int));
+		printf("%d", va_arg(args, int));
 
-			if (separator && i < n -1)
-				printf("%s", separator);
+		if (separator && i < n - 1)
+			printf("%s", separator);
 	}
 
 	printf("\n");

d7782f9d27375802c17afd6b90b5d8e72f2fb2f3
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-11-26 06:56:40 -0800

    A function that joins two strings and returns a pointer

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,7 +34,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s = malloc(sizeof(*s) * len);
 
 	if (s == NULL)
+	{
+		free(s);
 		return (NULL);
+	}
 
 	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];

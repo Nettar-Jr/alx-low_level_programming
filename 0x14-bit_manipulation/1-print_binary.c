520d93857212bcd47c6b30ad859f69b268bdb8cf
nettar-jr <alabiabdulmumuni9@gmail.com>
2022-12-14 02:15:10 -0800

    A function that converts a decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * print_binary - convert decimal to binary
@@ -10,11 +10,13 @@
 
 void print_binary(unsigned long int n)
 {
-	if(n >> 0)
+	if (n >> 0)
 	{
-		if(n >> 1)
+		if (n >> 1)
+		{
 			print_binary(n >> 1);
 			_putchar((n & 1) + '0');
+		}
 	}
 	else
 	{

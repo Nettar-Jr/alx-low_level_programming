ed28d15ace0c40b8318b8716a665f12a3cfd4751
nettar-jr <alabiabdulmumuni9@gmail.com>
2023-01-04 03:14:40 -0800

    A function that frees node memorey of a singly linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -14,8 +14,9 @@ void free_list(list_t *head)
 {
 	list_t *current_node;
 
-	while ((current_node = head) != NULL)
+	while (head != NULL)
 	{
+		current_node = head;
 		head = head->next;
 		free(current_node->str);
 		free(current_node);

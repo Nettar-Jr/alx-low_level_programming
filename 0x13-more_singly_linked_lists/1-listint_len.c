f3fb5efdb8da3aa36bec7dd26131afc85efd9e2c
nettar-jr <alabiabdulmumuni9@gmail.com>
2023-01-05 12:23:53 -0800

    A function that returns the number of nodes in a linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,11 +2,11 @@
 #include "lists.h"
 
 /**
+ * listint_len - prints the number of nodes in a list
  *
+ * @h: pointer the first node
  *
- *
- *
- *
+ * Return: size of node
  */
 
 size_t listint_len(const listint_t *h)
